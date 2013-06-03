d3db9dff4254453b074bb6c192e75d492333efec
Colin Powell <colin.powell@gmail.com>
2013-06-02 20:48:32 -0400

    Adding a few templates and updating artifacts urls.

--- artifacts/urls.py
+++ artifacts/urls.py
@@ -6,12 +6,12 @@ from .views import OrganizationDetailView, OrganizationListView, ArtifactDetailV
 
 # custom views vendors
 urlpatterns = patterns('market.views',
-    url(r'^$', view=OrganizationListView.as_view(), name="artifacts_index"),
     url(r'^whats-here/$', view=WhatsHereView.as_view(), name="whats_here"),
     url(r'^lookup-loc/(?P<loc>[-\w]+)/$', view=get_nearby_artifacts, name="lookup_loc"),
-    url(r'^(?P<slug>[-\w]+)/', view=OrganizationDetailView.as_view(), name="organization_detail"),
-    url(r'^(?P<organization_slug>[-\w]+)/artifacts/', view=ArtifactListView.as_view(), name="artifact_list"),
     url(r'^(?P<organization_slug>[-\w]+)/artifacts/(?P<slug>[-\w]+)/', view=ArtifactDetailView.as_view(), name="artifact_detail"),
-    url(r'^(?P<organization_slug>[-\w]+)/tours/', view=TourListView.as_view(), name="tour_list"),
+    url(r'^(?P<organization_slug>[-\w]+)/artifacts/', view=ArtifactListView.as_view(), name="artifact_list"),
     url(r'^(?P<organization_slug>[-\w]+)/tours/(?P<slug>[-\w]+)/', view=TourDetailView.as_view(), name="tour_detail"),
+    url(r'^(?P<organization_slug>[-\w]+)/tours/', view=TourListView.as_view(), name="tour_list"),
+    url(r'^(?P<slug>[-\w]+)/', view=OrganizationDetailView.as_view(), name="organization_detail"),
+    url(r'^$', view=OrganizationListView.as_view(), name="artifacts_index"),
 )
